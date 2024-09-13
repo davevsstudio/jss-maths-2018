5f3983c851775c687b6db2ee5c648e9196a75958
davestar <142616037+davevsstudio@users.noreply.github.com>
2024-09-12 22:22:09 -0700

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,301 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Write 20/8 in mixed fraction",
+    answer: "2½",
+    options: [
+      "2¾",
+      "2½",
+      "2¼",
+      "2 1⁄8"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Calculate the perimeter of a triangle is (cm) whose dismensions are 1m 60cm, 1m 80cm and 2m",
+    answer: "540cm",
+    options: [
+      "460cm",
+      "540cm",
+      "576cm",
+      "660cm"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The point of intersection of x and y axis in a graph is called a/an",
+    answer: "origin",
+    options: [
+      "cartesian",
+      "coordinate",
+      "origin",
+      "plane"
+    ]
+  },
+    {
+    numb: 4,
+    question: "In a toss of a fair die, the probablity of getting a five is",
+    answer: "1/6",
+    options: [
+      "1/2",
+      "1/6",
+      "1/5",
+      "5/6"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Simplify -8(-12)+(-2)",
+    answer: "+2",
+    options: [
+      "-22",
+      "+22",
+      "-2",
+      "+2"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Write the sum of MC and CD in Hindu-Arabic",
+    answer: "1500",
+    options: [
+      "1100",
+      "1300",
+      "1500",
+      "1700"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "Solve the simultaneous equations of x + y =8 and x - y =2 ",
+    answer: "pre-sale service",
+    options: [
+      "x=3, y=5",
+      "x=-3, y=-5",
+      "x=5, y=-3",
+      "x=5, y=3"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "Factorise 3xy + 6yz",
+    answer: "3y(x + 2z)",
+    options: [
+      "9xyz",
+      "36xyz",
+      "3y(xz + 2z)",
+      "3y(x + 2z)"
+    ]
+  }, 
+  {
+    numb: 9,
+    question: "Find the area of parallelogram with base 8cm and vetical height 5cm",
+    answer: "80cm2",
+    options: [
+      "80cm2",
+      "40cm2",
+      "26cm2",
+      "13cm2"
+    ]
+  },
+
+  {
+    numb: 10,
+    question: "A tennis ball is typical example of ",
+    answer: "sphere",
+    options: [
+      "circle",
+      "cone",
+      "hemisphere",
+      "sphere"
+    ]
+  },
+
+  {
+    numb: 11,
+    question: "if F is the number of Faces, E is the number of Edges and V is the number of Vertices, calculate V - E + F for a hexagonal prism",
+    answer: "2",
+    options: [
+      "2",
+      "4",
+      "6",
+      "8"
+    ]
+  },
+
+  {
+    numb: 12,
+    question: "_______ has an infinite number of lines of symmetry",
+    answer: "circle",
+    options: [
+      "circle",
+      "rectangle",
+      "square",
+      "equilteral triangle"
+    ]
+  },
+
+
+  {
+    numb: 13,
+    question: "Find the meddium of the folloing decimal number 0.1, 0.2, 0.3, 0.4, 0.6, 0.7 and 0.8",
+    answer: "0.4",
+    options: [
+      "0.1",
+      "0.3",
+      "0.4",
+      "0.6"
+    ]
+  },
+
+  {
+    numb: 14,
+    question: "The sum of angles in a pentgon is",
+    answer: "540®",
+    options: [
+      "360®",
+      "480®",
+      "540®",
+      "720®"
+    ]
+  },
+
+  {
+    numb: 15,
+    question: "Express 100111two in base ten",
+    answer: "39ten",
+    options: [
+      "32ten",
+      "36ten",
+      "38ten",
+      "39ten"
+    ]
+  },
+
+  {
+    numb: 16,
+    question: "Simplify 3 1⁄6 + 1 1⁄4 - 2 1⁄4",
+    answer: "1 3⁄4",
+    options: [
+      "2 3⁄4",
+      "2 1⁄4",
+      "1 3⁄4",
+      "1 1⁄4"
+    ]
+  },
+
+
+  {
+    numb: 17,
+    question: "A prime has only two factors which are ______ and ______",
+    answer: "itself and 1",
+    options: [
+      "1 and any other number",
+      "2 and 3",
+      "1 and 2",
+      "itself and 1"
+    ]
+  },
+
+  {
+    numb: 18,
+    question: "A men is x years old and his son is y year old. What is their average age?",
+    answer: "(x⁄2 + y⁄2)",
+    options: [
+      "(½ 2XY) years",
+      "(x + y) years",
+      "(x⁄2 + y⁄2) ",
+      "(x+y)/2xy"
+    ]
+  },
+
+  {
+    numb: 19,
+    question: "If the sum of even and prime number given even number, what is the actual value of the prime number?",
+    answer: "1",
+    options: [
+      "7",
+      "5",
+      "3",
+      "1"
+    ]
+  },
+
+
+
+  {
+    numb: 20,
+    question: "_______ divides a circle into equal parts",
+    answer: "diameter",
+    options: [
+      "arc",
+      "chord",
+      "diameter",
+      "radius"
+    ]
+  },
+
+
+  {
+    numb: 21,
+    question: "Simplify t [t - (1 + t)]",
+    answer: "1+t",
+    options: [
+      "1+t",
+      "t-1",
+      "1-t",
+      "t"
+    ]
+  },
+
+  {
+    numb: 22,
+    question: "Calculate the volume of a cube of length 8m",
+    answer: "512cm2",
+    options: [
+      "24cm2",
+      "512cm2",
+      "576cm2",
+      "800cm2"
+    ]
+  },
+
+  {
+    numb: 23,
+    question: "Find the reciprocal of 1⁄5 ",
+    answer: "5",
+    options: [
+      "-5",
+      "0.2",
+      "1⁄25",
+      "5"
+    ]
+  },
+
+  {
+    numb: 24,
+    question: "A trader made a profit of 35% on an article he brought for N5000.00. How much did he sell the article",
+    answer: "N6750.00",
+    options: [
+      "N1750.00",
+      "N3250.00",
+      "N6750.00",
+      "N7500.00"
+    ]
+  },
+
+  {
+    numb: 25,
+    question: "Find the difference between the media and the mode of the scores 4,3,5,9,4,6,3,8,6,4,5",
+    answer: "3",
+    options: [
+      "5",
+      "4",
+      "3",
+      "1"
+    ]
+  },
+];
